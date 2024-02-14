eb9345c8f8daf66f3e8f1e96df264e11dafec7f4
Jozef Nagy <schkwve@gmail.com>
2024-02-14 12:12:24 +0100

    opcodes

--- src/cpu.c
+++ src/cpu.c
@@ -7,9 +7,15 @@ struct cpu cpu = {0};
 
 void cpu_init(struct devtree devtree)
 {
-	// bios
-	cpu.pc = BIOS_START;
 	cpu.devtree = devtree;
+
+	for (int i = 0; i < 32; i++) {
+		cpu.regs[i] = 0;
+	}
+
+	cpu.pc = BIOS_START;
+	cpu.hi = 0;
+	cpu.lo = 0;
 }
 
 uint32_t cpu_fetch_instr(uint32_t addr)
@@ -23,13 +29,39 @@ uint32_t cpu_fetch_instr(uint32_t addr)
 
 void cpu_exec(uint32_t instr)
 {
-	switch (instr) {
+	switch (instr >> 26) {
+		case 0x0D: { // ori
+			uint32_t reg = (instr >> 16) & 0xFF;
+			uint32_t val = (instr & 0xFFFF);
+			uint32_t res = cpu_get_reg(reg) | val;
+			fprintf(stderr, "ori: (0x%x | $%d) -> %d (= 0x%x)\n", val, reg, reg, res);
+			cpu_set_reg(reg, res);
+			break;
+		}
+		case 0x0F: { // lui
+			uint16_t reg = (instr >> 16) & 0x1F;
+			uint32_t val = (instr & 0xFFFF) << 16;
+			cpu_set_reg(reg, val);
+			fprintf(stderr, "lui: 0x%x -> %d\n", val, reg);
+			break;
+		}
 		default: {
 			fprintf(stderr, "[psxe/error]: Unknown instruction! (0x%x)\n", instr);
 			die();
 			break;
 		}
 	}
+}
+
+void cpu_set_reg(uint8_t index, uint32_t val)
+{
+	cpu.regs[index] = val;
+	cpu.regs[0] = 0;
+}
+
+uint32_t cpu_get_reg(uint8_t index)
+{
+	return cpu.regs[index];
 }
 
 void cpu_cycle(void)
--- src/cpu.h
+++ src/cpu.h
@@ -6,7 +6,13 @@
 #include "devtree.h"
 
 struct cpu {
+	// gpregs
+	uint32_t regs[32];
+
+	// sregs
 	uint32_t pc;
+	uint32_t hi;
+	uint32_t lo;
 
 	struct devtree devtree;
 };
@@ -14,6 +20,9 @@ struct cpu {
 void cpu_init(struct devtree devtree);
 uint32_t cpu_fetch_instr(uint32_t addr);
 void cpu_exec(uint32_t instr);
+
+void cpu_set_reg(uint8_t index, uint32_t val);
+uint32_t cpu_get_reg(uint8_t index);
 
 void cpu_cycle(void);
 
